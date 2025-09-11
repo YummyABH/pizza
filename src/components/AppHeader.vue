87511e55cd6aaf380b762402e930b2aa67601c49
nikita.sidiropulo@a-mobile.biz <Yomo1Yomo>
2025-09-11 12:00:05 +0300

    feat(comment button): закомментирована кнопка в header'e

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,17 +10,17 @@ import IconMap from './icons/IconMap.vue'
       <div class="flex justify-between">
         <div class="font-bold text-3xl">ЛОГОТИП</div>
         <div class="flex gap-x-10 max-md:gap-x-5 items-center">
-          <div
+          <!-- <div
             class="px-6 py-3 font-bold max-md:hidden rounded-4xl hover:bg-gray-600 hover:text-current bg-white text-black duration-200 cursor-pointer text-xl"
           >
             Выбрать место доставки
-          </div>
+          </div> -->
 
-          <div
+          <!-- <div
             class="w-12 max-sm:p-2 max-sm:w-10 md:hidden border rounded-full p-3 duration-200 hover:bg-gray-600 cursor-pointer hover:border-gray-600"
           >
             <IconMap />
-          </div>
+          </div> -->
           <div
             class="w-12 max-sm:p-2 max-sm:w-10 border rounded-full p-3 duration-200 hover:bg-gray-600 cursor-pointer hover:border-gray-600"
           >
