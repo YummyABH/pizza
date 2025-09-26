7f435d429cc729caf04b630ca099f5472aa5b214
nikita.sidiropulo@a-mobile.biz <Yomo1Yomo>
2025-09-26 13:58:48 +0300

    feat(localStorage): добавил сохранение формирующегося заказа в localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue'
 import AppFooter from '@/components/AppFooter.vue'
+
+function generateSecretKey(length = 32) {
+  const array = new Uint8Array(length)
+  window.crypto.getRandomValues(array)
+  return btoa(String.fromCharCode(...array))
+}
+
+if (!localStorage.getItem('secretKey')) {
+  const secretKey = generateSecretKey()
+  localStorage.setItem('secretKey', secretKey)
+}
 </script>
 
 <template>
--- src/stores/orderStore.ts
+++ src/stores/orderStore.ts
@@ -1,52 +1,36 @@
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+import { reactive, watch } from 'vue'
 
 export const useOrderStore = defineStore('order', () => {
-  const order = reactive({
-    phone: '',
-    name: '',
-    delivery: {
+  const savedOrder = localStorage.getItem('order')
+  const initialOrder = savedOrder ? JSON.parse(savedOrder) : null
+
+  const order = reactive(
+    initialOrder || {
+      phone: '',
+      name: '',
+      delivery: {
+        status: true,
+        address: '',
+        comment: '',
+      },
+      dishes: [],
+      cutlery_status: false,
+      number_cutlery: 1,
+      payment_card: null,
+      order_comment: '',
       status: true,
-      address: '',
-      comment: '',
+      price: 0,
     },
-    dishes: [
-      {
-        quantity: 1,
-        id: 1,
-        name: 'Маргарита',
-        categoryName: 'Пицца',
-        price: 650,
-        description: 'Классическая пицца с томатным соусом и моцареллой',
-        dish_status: true,
-        composition: ['Томатный соус', 'Моцарелла', 'Базилик'],
-        dish_weight: '400',
-        image: 'margherita.jpg',
-        categoryid: 1,
-        size: ['30', '40'],
-      },
-      {
-        quantity: 1,
-        id: 2,
-        name: 'Пепперони',
-        categoryName: 'Пицца',
-        price: 800,
-        description: 'Пицца с острой колбасой пепперони и сыром',
-        dish_status: true,
-        composition: ['Томатный соус', 'Пепперони', 'Моцарелла'],
-        dish_weight: '450',
-        image: 'pepperoni.jpg',
-        categoryid: 1,
-        size: ['30', '40'],
-      },
-    ],
-    cutlery_status: false,
-    number_cutlery: 1,
-    payment_card: null,
-    order_comment: '',
-    status: true,
-    price: 0,
-  })
+  )
+
+  watch(
+    order,
+    (newOrder) => {
+      localStorage.setItem('order', JSON.stringify(newOrder))
+    },
+    { deep: true },
+  )
 
   function cutleryAdd() {
     return order.number_cutlery >= 50 ? (order.number_cutlery = 50) : order.number_cutlery++
