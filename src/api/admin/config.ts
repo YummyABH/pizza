c3713901fb65f17b9e4917cd86f984b6e2ae3059
YummyABH <harchilavaarsen2@gmail.com>
2025-10-17 08:03:29 +0300

    feat(adaptive): сделан адаптив для страицы заказов админа

--- src/api/admin/config.ts
+++ src/api/admin/config.ts
@@ -5,7 +5,7 @@ export const APIInstanceAdmin = $fetch.create({
   headers: {
     'Content-Type': 'application/json',
     authorization:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6MywidXNlcm5hbWUiOiJrZW5ueSIsInJvbGUiOjEsImlhdCI6MTc2MDYyMjk4MSwiZXhwIjoxNzYwNjIzODgxfQ.hoKTWIYq3ZRukrJ-abAkgaSiAWAgT2WAkpGl1mJS2Bg',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6MywidXNlcm5hbWUiOiJrZW5ueSIsInJvbGUiOjEsImlhdCI6MTc2MDYzOTcwOCwiZXhwIjoxNzYwNjQxNTA4fQ.Ba7MFDBw2LKM7ucyKK_gPlL9RPjNPXUXkyeTapjl0Sg',
   },
   async onResponse({ response }) {
     return response
